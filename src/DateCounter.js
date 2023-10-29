31b6c7b5fcf1b722925bedfaf6b2d87214d32f8f
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-29 15:39:46 +0330

    Remove Comment and Did Reset with Reducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
 
@@ -16,20 +18,15 @@ function reducer(state, action) {
     case "setStep":
       return { ...state, step: action.payload };
 
+    case "reset":
+      return initialState;
+
     default:
       throw new Error("Unknown action");
   }
-
-  // if (action.type === "inc") return state + 1;
-  // if (action.type === "dec") return state - 1;
-  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  // const [step, setStep] = useState(1);
-
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -39,29 +36,22 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({ type: "inc" });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     dispatch({ type: "setCount", payload: Number(e.target.value) });
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
     dispatch({ type: "setStep", payload: Number(e.target.value) });
-    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
