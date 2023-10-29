a63297cc2d58f6a6a9a4fa6d710b573b91aaa741
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-29 18:14:31 +0330

    Go to Next Question

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Loader from "./Loader";
 import Main from "./Main";
 import Error from "./Error";
+import NextButton from "./NextButton";
 import { useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
@@ -50,6 +51,13 @@ function reducer(state, action) {
             : state.points,
       };
 
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
     default:
       throw new Error("Action Unknown");
   }
@@ -81,11 +89,14 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
