764c014eff9571d2beda22ed24a65f9418041ee8
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-29 17:00:21 +0330

    Implement JsonServer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFaild":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFaild" }));
+  });
+
   return (
     <div className="app">
       <Header />
