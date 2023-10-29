8a638f55439892da31d7dd4d5b69f86f1eead795
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-30 00:04:06 +0330

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         {/* if non of above won't find */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
