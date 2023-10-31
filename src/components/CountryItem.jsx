be2911416e2d45c588182be3fcc7239cfc4a85c9
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-31 12:38:31 +0330

    Implement Countries

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -3,7 +3,7 @@ import styles from "./CountryItem.module.css";
 function CountryItem({ country }) {
   return (
     <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
+      <img src={country.emoji} alt={country.country} />
       <span>{country.country}</span>
     </li>
   );
