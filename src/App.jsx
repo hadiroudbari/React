9c2e12abd9baeca7afb91ae6741498f8d3bd83ba
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-31 11:20:23 +0330

    Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         {/* if non of above won't find */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
