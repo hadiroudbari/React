e96bd7f330ec363afddf76d20087915edbdbeea5
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-02 22:57:31 +0330

    Deleting City

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -50,9 +50,24 @@ function CitiesProvider({ children }) {
       });
       const data = await res.json();
 
-      setCities((cities) => [...cities, newCity]);
+      setCities((cities) => [...cities, data]);
     } catch {
-      alert("There was an error loading data...");
+      alert("There was an error creating city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city...");
     } finally {
       setIsLoading(false);
     }
@@ -66,6 +81,7 @@ function CitiesProvider({ children }) {
         currentCity,
         getCity,
         createCity,
+        deleteCity,
       }}
     >
       {children}
