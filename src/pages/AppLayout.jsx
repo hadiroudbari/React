28ab0e112f33b87dd1fefb218769716cb1fb9149
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-03 00:40:23 +0330

    Protected Route

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,6 +2,7 @@ import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 import styles from "./AppLayout.module.css";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppLayout() {
   return (
