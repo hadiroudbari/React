49338acd9e60fed156d651c174ddc4a30560d291
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-16 22:10:37 +0330

    Duplicate Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -40,6 +40,7 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // 2. Upload Image
+  if (hasImagePath) return;
 
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
