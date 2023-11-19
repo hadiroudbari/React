1e49c8269606a2d3d1ddbe3edc04038bcd162a5b
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-19 17:40:46 +0330

    Fix QueryData Cache Bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       // set to cache query
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
 
