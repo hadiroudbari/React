6c3cd299bb2e7be40d95f53e4f6efe10a874a6b3
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-19 23:56:45 +0330

    Fix Some Bugs

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -94,6 +94,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -112,7 +113,7 @@ function Toggle({ id }) {
 
 function List({ children, id }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
