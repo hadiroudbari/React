1c193db04dd48482c13a0cce7e0ecc812a03b390
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-19 17:27:26 +0330

    User Logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,7 +12,7 @@ export function useLogin() {
     onSuccess: (user) => {
       // set to cache query
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
