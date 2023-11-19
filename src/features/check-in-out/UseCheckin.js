3be59283774ab282e0d02f137202a428b50b375a
Hadiroudbari <hadiroudbari@gmail.com>
2023-11-19 12:46:50 +0330

    Cheking in a Booking

---
+++ src/features/check-in-out/UseCheckin.js
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function useCheckin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: checkin, isLoading: isCheckinIn } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-in",
+        isPaid: true,
+      }),
+
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked in`);
+      // active : true will invalidate all queries in page in case we can't remember the query name
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checkin in"),
+  });
+
+  return { checkin, isCheckinIn };
+}
